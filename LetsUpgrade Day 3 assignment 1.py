b0ebe968acbe326c6c2a08b337ce13f516bb37ae
Wuonam <106992597+Wuonam04@users.noreply.github.com>
2022-11-05 00:39:15 +0100

    Add files via upload
    first commit on day 3 assignment 1

---
+++ LetsUpgrade Day 3 assignment 1.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+#write a program to find those numbers which are divisible by 15 and multiples of 5,betweeen 10 and 150
+for i in range(10,151):
+    if i % 15 == 0 and i*5 % 5 == 0:
+        print(i, end = " ")
+        
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
