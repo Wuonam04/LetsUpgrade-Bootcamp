4be37d58ebbd234e1803beeb6e01c344b35f8243
Wuonam <106992597+Wuonam04@users.noreply.github.com>
2022-11-05 18:21:27 +0100

    Add files via upload
    day 3 assigment 2

---
+++ LetsUpgrade Day 3 assignment 2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#write a python program to print 100 prime numbers and you sould print "Hey bro i'm done"
+x = 200
+for i in range(2,x):       #1 is not a prime number
+    if i % 2 != 0 or i == 2:
+        print(i, end = " " )
+        
+print("Hey bro done")
+
+
+# In[ ]:
+
+
+
+
