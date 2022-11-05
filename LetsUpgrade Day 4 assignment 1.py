99f554bdd8d7678ccba8624d47e232a9dca41e8c
Wuonam <106992597+Wuonam04@users.noreply.github.com>
2022-11-05 19:03:20 +0100

    Add files via upload
    Day 4 assignment

---
+++ LetsUpgrade Day 4 assignment 1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#Write a python program to reverse a string
+string = "LetsUpgrade"
+rev = ""
+for i in string:    
+    rev = i + rev
+print(rev)
+
+
+# In[ ]:
+
+
+
+
